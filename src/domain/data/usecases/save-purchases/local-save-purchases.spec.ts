6c51c7a389f11232163ec7be593962b6305352e3
Leonardo Tsuji <leonardo.tsuji@sotran.com.br>
2021-10-27 19:17:36 -0300

    refatorando

---
+++ src/domain/data/usecases/save-purchases/local-save-purchases.spec.ts
@@ -0,0 +1,42 @@
+import { CacheStore } from "@/data/protocols/cache";
+import { LocalSavePurchases } from "@/data/usecases";
+
+class CacheStoreSpy implements CacheStore {
+  deleteCallsCount = 0;
+  key: string;
+
+  delete(key: string): void {
+    this.deleteCallsCount++;
+    this.key = key;
+  }
+}
+
+type SutTypes = {
+  sut: LocalSavePurchases;
+  cacheStore: CacheStoreSpy;
+};
+//Factory
+const makeSut = (): SutTypes => {
+  const cacheStore = new CacheStoreSpy();
+  const sut = new LocalSavePurchases(cacheStore);
+
+  return {
+    cacheStore,
+    sut,
+  };
+};
+
+describe("LocalSavePurchases", () => {
+  test("Should not delete cache on sut.init", () => {
+    const { cacheStore } = makeSut();
+    new LocalSavePurchases(cacheStore);
+    expect(cacheStore.deleteCallsCount).toBe(0);
+  });
+
+  test("Should delete old cache on sut.save", async () => {
+    const { cacheStore, sut } = makeSut();
+    await sut.save();
+    expect(cacheStore.deleteCallsCount).toBe(1);
+    expect(cacheStore.key).toBe("purchases");
+  });
+});
